cd5f5880e98c45a1cbd80737c4644dce56854da1
Ashkan Ganj <ashkanganj@gmail.com>
2024-10-26 17:11:11 -0400

    update padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <main>
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-5">
         <h1 className="text-4xl">Hello World</h1>
       </div>
     </main>
