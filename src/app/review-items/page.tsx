7eaa759d748f5b4d11d9592d7aa5095ca242d64a
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2024-11-06 08:05:30 -0500

    Completed review-item-specific-item page. Completed Remove Inactive Item, Publish Item, and Unpublish Item Use Cases

--- src/app/review-items/page.tsx
+++ src/app/review-items/page.tsx
@@ -1,24 +1,26 @@
 'use client'
-import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ReviewItemCardComponent from './ReviewItemCardComponent'
-import Link from 'next/link';
+import { useRouter } from "next/navigation";
+import React from "react";
 import ItemFilter from "./ItemFilter";
+import ReviewItemCardComponent, { ReviewItemCardComponentProps } from './ReviewItemCardComponent';
 const baseURL = "https://ziek69aur9.execute-api.us-east-2.amazonaws.com/initial";
 
 export default function ReviewItems() {
     const [userType, setUserType] = React.useState<string | null>(null);
     const [userName, setUserName] = React.useState<string | null>(null);
-    const [passowrd, setPassowrd] = React.useState<string | null>(null); 
-    const [items, setItems] = useState<any[]>([]);
+    const [password, setPassword] = React.useState<string | null>(null);
+    const [items, setItems] = React.useState<any[]>([]);
     const [chosenFilter, setChosenFilter] = React.useState<string>('');
+    const router = useRouter();
+
     React.useEffect(() => {
         setUserType(sessionStorage.getItem('userType'));
         setUserName(sessionStorage.getItem('userName'));
-        setPassowrd(sessionStorage.getItem('password'));
+        setPassword(sessionStorage.getItem('password'));
 
         const getSellerItems = async () => {
-            const response = await axios.post(`${baseURL}/review-items`,{
+            const response = await axios.post(`${baseURL}/review-items`, {
                 body: {
                     username: sessionStorage.getItem('userName'),
                     password: sessionStorage.getItem('password'),
@@ -31,22 +33,28 @@ export default function ReviewItems() {
         getSellerItems();
     }, []);
 
-    const filteredItems = items.filter((item: any) => 
+    const filteredItems = items.filter((item: any) =>
         chosenFilter === '' || item.ActivityStatus === chosenFilter);
 
-  return (
-    <main >
-        <div className="container mx-auto mt-5">
-            <h1 className="text-4xl mb-6">Review Items Page</h1>
-            <ItemFilter {...{ chosenFilter, setChosenFilter}}/>
-            <div className="grid grid-cols-4 gap-4">
-                {filteredItems.map((item, index) => (
-                    <Link href = "/review-item-specific-item" key = {index}>
-                         <ReviewItemCardComponent {...item}  />
-                    </Link>
-                ))}
+    return (
+        <main >
+            <div className="container mx-auto mt-5">
+                <h1 className="text-4xl mb-6">Review Items Page</h1>
+                <ItemFilter {...{ chosenFilter, setChosenFilter }} />
+                <div className="grid grid-cols-4 gap-4">
+                    {filteredItems.map((item: ReviewItemCardComponentProps, index: number) => (
+                        <div
+                            key={index}
+                            onClick={() => {
+                                console.log('Clicked on item with ID ', item.ItemID);
+                                sessionStorage.setItem('selectedItemID', item.ItemID.toString());
+                                router.push('/review-items/specific-item');
+                            }}>
+                            <ReviewItemCardComponent {...item} />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    </main>
-  );
+        </main>
+    );
 }
