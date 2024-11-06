a6248d39695b18a2e3debf22d6bb2022b3b152f2
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2024-11-06 15:53:35 -0500

    Finished edit item use case

--- src/app/review-items/specific-item/page.tsx
+++ src/app/review-items/specific-item/page.tsx
@@ -310,7 +310,11 @@ export default function Home() {
                         </button>}
                     {activityStatus?.toLowerCase() === "inactive" &&
                         <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={() => {
+                                sessionStorage.setItem('editItemID', itemID!.toString());
+                                router.push('/review-items/specific-item/edit-item');
+                            }}>
                             Edit Details
                         </button>}
                 </div>
