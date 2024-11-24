30fe10387247b70e863815f61c7b212c8f132b38
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2024-11-24 12:38:34 -0500

    Resolved merge conflict

--- src/app/add-item/page.tsx
+++ src/app/add-item/page.tsx
@@ -161,16 +161,6 @@ export default function AddItem() {
           <div className="grid gap-6 mb-6 md:grid-cols-1">
             <div>
               <label htmlFor="is_buy_now" className="inline-flex items-center cursor-pointer">
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-4">Is it a Buy-Now item?</span>
-                <input type="checkbox" id="is_buy_now" checked={isBuyNow === 1} onChange={() => {
-                  isBuyNow == 1 ? setIsBuyNow(0) : setIsBuyNow(1);
-                }} className="sr-only peer"></input>
-
-                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="is_buy_now" className="inline-flex items-center cursor-pointer">
                 <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-4">Is it a Buy-Now item? Toggled on = Yes. Toggled off = No.</span>
                 <input type="checkbox" id="is_buy_now" checked={isABuyNow === 1} onChange={() => {
                   isABuyNow == 1 ? setIsABuyNow(0) : setIsABuyNow(1);
