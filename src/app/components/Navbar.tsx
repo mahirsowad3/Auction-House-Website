76efcd33ca828cccb83a87aecbbfc00dbdaa1afa
Ashkan Ganj <ashkanganj@gmail.com>
2024-11-26 15:41:26 -0500

    updated the font size

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -224,8 +224,8 @@ const Navbar = () => {
                 </div>
             </nav>
             <div className="container mx-auto mt-6">
-                <h1 className="text-3xl text-gray-900">
-                    {userName ? `Welcome ${userName}` : 'Welcome'}
+                <h1 className="text-5xl text-gray-900">
+                    {userName ? ` Welcome ${userName}` : 'Welcome'}
                 </h1>
             </div>
         </div>
