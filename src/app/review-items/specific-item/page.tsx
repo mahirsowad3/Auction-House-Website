8865a3feae929f0a9e70bd1ce4ffbfeb18bce30d
12axel <116901882+12axel@users.noreply.github.com>
2024-12-07 12:52:24 -0500

    Another date bug fix

--- src/app/review-items/specific-item/page.tsx
+++ src/app/review-items/specific-item/page.tsx
@@ -411,7 +411,6 @@ export default function Home() {
                         <h2 className="text-2xl">Sold Date: </h2>
                         <p className="text-xl">{soldDate ? new Date(soldDate.replace(' ', 'T'))
                             .toLocaleDateString('en-US', {
-                                timeZone: 'UTC',
                                 month: '2-digit',
                                 day: '2-digit',
                                 year: 'numeric',
